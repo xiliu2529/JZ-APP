8092a69454dc3639f2023e24c581b4d45f8c5501
lthuang-ai <130113225+xiliu2529@users.noreply.github.com>
2026-03-12 17:27:47 +0900

    版本一致

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth } from 'firebase/auth';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { getReactNativePersistence } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-import { Platform } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-AGz8yMIyEim-25rsVRr3fzNn8qJGHUw",
@@ -19,12 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// Web 平台用 getAuth，原生平台用 AsyncStorage 持久化
-export const auth = Platform.OS === 'web'
-  ? getAuth(app)
-  : initializeAuth(app, {
-      persistence: getReactNativePersistence(AsyncStorage),
-    });
-
-export const database =getDatabase(app, "https://jz-app-15152-default-rtdb.asia-southeast1.firebasedatabase.app")
+export const auth = getAuth(app);
+export const database = getDatabase(app, "https://jz-app-15152-default-rtdb.asia-southeast1.firebasedatabase.app");
 export default app;
